43608b12f9e5df9e8b1f5e6287613beeb63c9b3e
james <jamesluck@justgiving.com>
2014-11-13 16:41:05 +0000

    Validation fixed, slight tidy

--- src/client/addCourse/templates/addCourse.js
+++ src/client/addCourse/templates/addCourse.js
@@ -1,41 +1,28 @@
-Template.addCourse.courseFormats = function(){
-	return _.map(
-		Collections.CourseFormats.find().fetch(),
-		function(e){
-			return { 
-				label: e.type, 
-				value: e.type
-			};
-		}
-	);
-};
+Template.addCourse.helpers({
+	courseFormats : function(){
+		return _.map(
+			Collections.CourseFormats.find().fetch(),
+			function(e){
+				return { 
+					label: e.type, 
+					value: e.type
+				};
+			}
+		);
+	}
+});
 
 Template.addCourse.events = {
 	"click #btnAddCourse" : function(event, template){
 		
 		var isValid = AutoForm.validateForm("addCourse")
 		_.each(Sessions.getSessions(), function(e){
-			isValid = isValid && AutoForm.validateForm(e.formId)	
+			console.log(e);
+			isValid = AutoForm.validateForm(e.formId) && isValid;
 		});
 		
 		if (isValid){
 			//insert course and session
 		}
 	}
-}
-
-Template.sessionDetails.locations = function(){
-	return _.map(
-		Collections.Locations.find().fetch(), 
-		function(e){
-			return {
-				label : e.name,
-				value : e.name
-			};
-		}
-	);
-}
-
-Template.sessionDetails.canBeRemoved = function(){
-	return !this.isFirst;
 }
